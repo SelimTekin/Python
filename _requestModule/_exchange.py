46b1094ff8181864ffd75dad23ab8f3a0bf5f835
SelimTekin <tekinselim.57.57@gmail.com>
2022-06-24 19:04:05 +0300

    .

--- _requestModule/_exchange.py
+++ _requestModule/_exchange.py
@@ -1,15 +1,41 @@
 import requests
 import json
 
-api_url = "https://api.exchangeratesapi.io/latest?base="
+# API_KEY kısmına siteye kaydolup api key alıp yazmanız lazım
+# api_url = "https://api.exchangeratesapi.io/v1/latest?access_key=soJKYZq4ern4UofbC2VKpqVRfCbED4Ac&base=" # eşittirden sonraki kısım boş olsun ki istediğimizi girebilelim(normalde USD yazıyordu (dolar))
+
 
 bozulan_doviz = input("bozulan döviz türü: ")
 alinan_doviz = input("alınan döviz türü: ")
 miktar = int(input(f"Ne kadar {bozulan_doviz} bozdurmak istiyorsunuz: "))
 
-result = requests.get(api_url+bozulan_doviz)
-result = json.loads(result.text)
+api_url = f"https://api.apilayer.com/exchangerates_data/convert?to={alinan_doviz}&from={bozulan_doviz}&amount={miktar}"
 
-print("1 {0} = {1} {2}".format(bozulan_doviz, result["rates"][alinan_doviz], alinan_doviz))
-print("{0} {1} = {2} {3}".format(miktar, bozulan_doviz, miktar * result["rates"][alinan_doviz],alinan_doviz))
+payload = {}
+headers= {
+  "apikey": "soJKYZq4ern4UofbC2VKpqVRfCbED4Ac"
+}
 
+
+response = requests.request("GET", api_url, headers=headers, data = payload)
+
+status_code = response.status_code
+result = response.text
+result = json.loads(result)
+
+print(result["result"])
+# print("1 {0} = {1} {2}".format(bozulan_doviz,result["rates"][alinan_doviz], alinan_doviz))
+# print("{0} {1} = {2} {3}".format(miktar, bozulan_doviz,miktar * result["rates"][alinan_doviz], alinan_doviz))
+
+
+# url = "https://api.apilayer.com/exchangerates_data/convert?to=TRY&from=USD&amount=amount"
+
+# payload = {}
+# headers= {
+#   "apikey": "soJKYZq4ern4UofbC2VKpqVRfCbED4Ac"
+# }
+
+# response = requests.request("GET", url, headers=headers, data = payload)
+
+# status_code = response.status_code
+# result = response.text
--- _requestModule/github.py
+++ _requestModule/github.py
@@ -3,48 +3,47 @@ import requests
 class Github:
     def __init__(self):
         self.api_url = 'https://api.github.com'
-        self.token = '<token_bilgisi>'
-
+        self.token = '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'
+        self.headers= {"apikey": "soJKYZq4ern4UofbC2VKpqVRfCbED4Ac"}
     def getUser(self, username):
-        response = requests.get(self.api_url+'/users/'+ username)
+        response = requests.get(self.api_url+'/users/'+username)
+        return response.json() # json.loads() ile aynı
+
+    def getRepositories(self, username):
+        response = requests.get(self.api_url+'/users/'+username+'/repos')
         return response.json()
 
-    def getRepositories(self,username):
-        response = requests.get(self.api_url+'/users/'+ username+'/repos')
-        return response.json()
-
-    def createRepository(self, name):
-        response = requests.post(self.api_url+'/user/repos?access_token='+ self.token, json={
-            "name": name,
-            "description": "This is your first repository",
-            "homepage": "https://sadikturan.com",
+    def createRepository(self, repoName):
+        response = requests.post(self.api_url+'/user/repos?access_token='+self.token, json={
+            "name": repoName,
+            "description": "This is a test creating in Github",
+            "homepage": "https://github.com",
             "private": False,
             "has_issues": True,
             "has_projects": True,
             "has_wiki": True
-        })
+        })        
+
         return response.json()
-
 github = Github()
-
 while True:
-    secim = input('1- Find User\n2- Get Repositories\n3- Create Repository\n4- Exit\nSeçim: ')
+    secim = input("1 - Find User\n2 - Get Repositories\n3 - Create Repository\n4 - Exit\n5 - Seçim: ")
 
     if secim == '4':
         break
     else:
         if secim == '1':
-            username= input('username: ')
+            username = input('username: ')
             result = github.getUser(username)
-            print(f"name: {result['name']} public repos: {result['public_repos']}  follower : {result['followers']}")
+            print(f"name: {result['name']} public repos: {result['public_repos']} followers: {result['followers']}")
         elif secim == '2':
             username = input('username: ')
             result = github.getRepositories(username)
             for repo in result:
-                print(repo['name'])    
+                print(repo["name"])
         elif secim == '3':
-            name = input('repository name: ')
-            result = github.createRepository(name)
-            print(result) 
+            repoName = input('Repository adı: ')
+            result = github.createRepository(repoName)
+            print(result)
         else:
-            print('yanlış seçim') 
+            print("Yanlış seçim")
