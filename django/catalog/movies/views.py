533e819892d3c7ed701e6ba9b53690812c5b3269
SelimTekin <tekinselim.57.57@gmail.com>
2022-09-14 20:00:37 +0300

    .

--- django/catalog/movies/views.py
+++ django/catalog/movies/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
 
 # render edeceğimiz sayfa template klasörü altındaki (template yazılmıyor) movies klasörü altındaki list.html sayfası
 def index(request):
-    return render(request, 'movies/list.html')
+    movies = Movie.objects.all()
+    context = {
+        'movies': movies
+    }
+    return render(request, 'movies/list.html', context)
 
 def detail(request):
     return render(request, 'movies/detail.html')
